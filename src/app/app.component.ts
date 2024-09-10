9a87d7f6f0de7322c2aaa7732c6b8f944c47b2ec
405141-Ziade-Alejandro <alejandroziade@gmail.com>
2024-09-10 15:44:20 -0300

    tring to make it work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,46 +4,78 @@ import { RouterOutlet } from '@angular/router';
 import { FileItem, FileType } from '../models/file.item.model';
 import { FILE_LIST } from '../data/file.storage';
 import { DatePipe } from '@angular/common';
+import { retry } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatePipe],
+  imports: [RouterOutlet, DatePipe, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   files: FileItem[] = FILE_LIST;
-  selected: FileItem[] = []
+  selected: FileItem[] = [];
+  selectedAction: string = '';
 
-
-  ngOnInit(){
-    this.sorting()
+  ngOnInit() {
+    this.sorting();
     console.log(this.files);
-
-  }
-  
-  sorting(){
-    const folders = this.files.filter(item=> item.type===FileType.FOLDER)
-    const file = this.files.filter(item=> item.type===FileType.FILE)
-
-    folders.sort((a,b)=>a.name.localeCompare(b.name))
-    file.sort((a,b)=> a.name.localeCompare(b.name))
-
-    this.files = [...folders,...file]
   }
 
-  logCheck(item:  FileItem) {
-    console.log('checked');
+  sorting() {
+    const folders = this.files.filter((item) => item.type === FileType.FOLDER);
+    const file = this.files.filter((item) => item.type === FileType.FILE);
 
-    //i need a way to remove it when it click again 
+    folders.sort((a, b) => a.name.localeCompare(b.name));
+    file.sort((a, b) => a.name.localeCompare(b.name));
 
-    if (!this.selected.includes(item)) {
-      this.selected.push(item)
+    this.files = [...folders, ...file];
+  }
+
+  logCheck(item: number) {
+    if (!this.selected.includes(this.files[item])) {
+      this.selected.push(this.files[item]);
+      console.log('added: ' + this.files[item]);
+
+      console.log(this.selected);
+      return;
     }
-    
-    console.log(this.selected);
-    
+    if (this.selected.includes(this.files[item])) {
+      this.selected = this.selected.filter(
+        (owner) => owner !== this.files[item]
+      );
+      console.log('removed: ' + this.files[item]);
+
+      console.log(this.selected);
+      return;
+    }
   }
-  title = 'file-management';
+
+  dlete() {
+    this.selected.forEach((element) => {
+      for (let index = 0; index < this.files.length; index++) {
+        if (this.files[index] === element) {
+          this.files = this.files.filter((a) => a !== this.files[index]);
+          console.log('found one');
+          console.log(this.files[index]);
+        }
+      }
+    });
+    console.log(this.files);
+    this.selected = [];
+  }
+
+  resetSelect(): void {
+    console.log(this.selectedAction);
+
+    if (this.selectedAction === 'delete') {
+      this.dlete();
+    } else if (this.selectedAction === 'add') {
+      //nothing for now
+    }
+    this.selectedAction = ''; // this isn't resetting the combobox
+    console.log(this.selectedAction);
+  }
 }
